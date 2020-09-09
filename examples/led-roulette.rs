e1044a33f97033f2f21db940f524824ba71589a1
Alexander Batischev <eual.jp@gmail.com>
2020-09-10 01:01:21 +0300

    Implement switch-hal abstraction for LEDs
    Compared to the API provided by stm32f429i-disc, this API doesn't hog
    the whole GPIOD, and even allows to use just one or two pins, leaving
    the others to be used for their alternative functions.

--- examples/led-roulette.rs
+++ examples/led-roulette.rs
@@ -6,7 +6,7 @@ use stm32f411e_disc as board;
 
 use board::hal::{delay, prelude::*, stm32};
 use board::{
-    led::{self, LedColor},
+    led::{self, OutputSwitch},
     peripheral,
 };
 
@@ -23,20 +23,30 @@ fn main() -> ! {
     let clocks = rcc.cfgr.freeze();
     let mut delayer = delay::Delay::new(syst, clocks);
 
-    let order = [
-        LedColor::Orange,
-        LedColor::Red,
-        LedColor::Blue,
-        LedColor::Green,
-    ];
+    let gpiod = board_peripherals.GPIOD.split();
 
-    let mut leds = led::Leds::new(board_peripherals.GPIOD.split());
+    let mut green_led = led::into_green_led(gpiod.pd12);
+    let mut orange_led = led::into_orange_led(gpiod.pd13);
+    let mut red_led = led::into_red_led(gpiod.pd14);
+    let mut blue_led = led::into_blue_led(gpiod.pd15);
 
-    for (prev, cur) in order.iter().cycle().zip(order.iter().cycle().skip(1)) {
-        leds[*prev].off();
-        leds[*cur].on();
-        delayer.delay_ms(100u32);
+    let delay: u32 = 100; // milliseconds
+
+    loop {
+        green_led.off().ok(); // TODO: replace by into_ok() once it's stabilized https://github.com/rust-lang/rust/issues/61695
+        orange_led.on().ok();
+        delayer.delay_ms(delay);
+
+        orange_led.off().ok();
+        red_led.on().ok();
+        delayer.delay_ms(delay);
+
+        red_led.off().ok();
+        blue_led.on().ok();
+        delayer.delay_ms(delay);
+
+        blue_led.off().ok();
+        green_led.on().ok();
+        delayer.delay_ms(delay);
     }
-
-    unreachable!("The infinite loop over LEDS terminated");
 }
--- src/led.rs
+++ src/led.rs
@@ -1,139 +1,40 @@
 //! On-board user LEDs.
 
-// Adapted from stm32f407g-disc crate.
-
-use crate::hal::prelude::*;
-
-use crate::hal::gpio::gpiod::{self, PD, PD12, PD13, PD14, PD15};
+use crate::hal::gpio::gpiod::{PD12, PD13, PD14, PD15};
 use crate::hal::gpio::{Output, PushPull};
 
-/// Top LED (orange)
-pub type LD3 = PD12<Output<PushPull>>;
+use switch_hal::{ActiveHigh, IntoSwitch, Switch};
 
-/// Left LED (green)
-pub type LD4 = PD13<Output<PushPull>>;
+pub use switch_hal::OutputSwitch;
 
-/// Right LED (red)
-pub type LD5 = PD15<Output<PushPull>>;
+// TODO: provide a convenient interface to get all LEDs at once:
+//
+//  fn helper(&mut gpiod) -> (green, orange, red, blue)
+//
+// TODO: provide a trait `Led` so users (like led-roulette) can make an array of LEDs and iterate
+// over them.
+//
+// TODO: perhaps replace into_<colour>_led() with a single into_led()? The benefits of the multiple
+// names are marginal, what are the chances that the user doesn't know which colour the LED they're
+// trying to obtain? What's the cost of that mistake compared to the effort of learning four
+// functions instead of one?
 
-/// Bottom LED (blue)
-pub type LD6 = PD14<Output<PushPull>>;
-
-/// User LED colors
-#[derive(Clone, Copy)]
-pub enum LedColor {
-    /// Green LED / LD4
-    Green,
-    /// Orange LED / LD3
-    Orange,
-    /// Red LED / LD5
-    Red,
-    /// Blue LED / LD6
-    Blue,
+/// Create a switch for the green LED (`LD4`).
+pub fn into_green_led<T>(pin: PD12<T>) -> Switch<PD12<Output<PushPull>>, ActiveHigh> {
+    pin.into_push_pull_output().into_active_high_switch()
 }
 
-/// Array of the on-board user LEDs
-pub struct Leds {
-    leds: [Led; 4],
+/// Create a switch for the orange LED (`LD3`).
+pub fn into_orange_led<T>(pin: PD13<T>) -> Switch<PD13<Output<PushPull>>, ActiveHigh> {
+    pin.into_push_pull_output().into_active_high_switch()
 }
 
-impl Leds {
-    pub fn new(gpiod: gpiod::Parts) -> Self {
-        let top = gpiod.pd12.into_push_pull_output();
-        let left = gpiod.pd13.into_push_pull_output();
-        let right = gpiod.pd14.into_push_pull_output();
-        let bottom = gpiod.pd15.into_push_pull_output();
-
-        Leds {
-            leds: [top.into(), left.into(), right.into(), bottom.into()],
-        }
-    }
-
-    pub fn iter_mut(&mut self) -> core::slice::IterMut<Led> {
-        self.leds.iter_mut()
-    }
+/// Create a switch for the red LED (`LD5`).
+pub fn into_red_led<T>(pin: PD14<T>) -> Switch<PD14<Output<PushPull>>, ActiveHigh> {
+    pin.into_push_pull_output().into_active_high_switch()
 }
 
-impl core::ops::Deref for Leds {
-    type Target = [Led];
-
-    fn deref(&self) -> &[Led] {
-        &self.leds
-    }
-}
-
-impl core::ops::DerefMut for Leds {
-    fn deref_mut(&mut self) -> &mut [Led] {
-        &mut self.leds
-    }
-}
-
-impl core::ops::Index<usize> for Leds {
-    type Output = Led;
-
-    fn index(&self, i: usize) -> &Led {
-        &self.leds[i]
-    }
-}
-
-impl core::ops::Index<LedColor> for Leds {
-    type Output = Led;
-
-    fn index(&self, c: LedColor) -> &Led {
-        &self.leds[c as usize]
-    }
-}
-
-impl core::ops::IndexMut<usize> for Leds {
-    fn index_mut(&mut self, i: usize) -> &mut Led {
-        &mut self.leds[i]
-    }
-}
-
-impl core::ops::IndexMut<LedColor> for Leds {
-    fn index_mut(&mut self, c: LedColor) -> &mut Led {
-        &mut self.leds[c as usize]
-    }
-}
-
-/// One of the on-board user LEDs
-pub struct Led {
-    pin: PD<Output<PushPull>>,
-}
-
-macro_rules! ctor {
-	($($ldx:ident),+) => {
-		$(
-			impl Into<Led> for $ldx {
-				fn into(self) -> Led {
-					Led {
-						pin: self.downgrade(),
-					}
-				}
-			}
-		)+
-	}
-}
-
-ctor!(LD3, LD4, LD5, LD6);
-
-impl Led {
-    /// Turns the LED off
-    pub fn off(&mut self) {
-        self.pin.set_low().ok();
-    }
-
-    /// Turns the LED on
-    pub fn on(&mut self) {
-        self.pin.set_high().ok();
-    }
-
-    /// Toggles the LED
-    pub fn toggle(&mut self) {
-        if let Ok(true) = self.pin.is_low() {
-            self.pin.set_high().ok();
-        } else {
-            self.pin.set_low().ok();
-        }
-    }
+/// Create a switch for the blue LED (`LD6`).
+pub fn into_blue_led<T>(pin: PD15<T>) -> Switch<PD15<Output<PushPull>>, ActiveHigh> {
+    pin.into_push_pull_output().into_active_high_switch()
 }
