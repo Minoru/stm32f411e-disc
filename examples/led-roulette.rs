a92b620213d25f37d35cc14155a16e5c2fd359bb
Alexander Batischev <eual.jp@gmail.com>
2020-09-09 20:42:30 +0300

    Add a simplistic LED roulette
    It uses busy-waiting for delays, which is suboptimal. On to learning
    timers!

---
+++ examples/led-roulette.rs
@@ -0,0 +1,39 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use stm32f411e_disc as board;
+
+use board::hal::{prelude::*, stm32};
+
+fn delay(cycles: u32) {
+    for _ in 0..cycles {}
+}
+
+#[board::entry]
+fn main() -> ! {
+    let board_peripherals = stm32::Peripherals::take()
+        .expect("board peripherals are already taken at the start of the program");
+
+    // User-programmable LEDs are connected to pins PD12 through PD15.
+    let gpiod = board_peripherals.GPIOD.split();
+    let mut green = gpiod.pd12.into_push_pull_output(); // LD4
+    let mut orange = gpiod.pd13.into_push_pull_output(); // LD3
+    let mut red = gpiod.pd14.into_push_pull_output(); // LD4
+    let mut blue = gpiod.pd15.into_push_pull_output(); // LD5
+
+    loop {
+        blue.set_low().expect("failed to set blue LED to low");
+        green.set_high().expect("failed to set green LED to high");
+        delay(3000);
+        green.set_low().expect("failed to set green LED to high");
+        orange.set_high().expect("failed to set orange LED to high");
+        delay(3000);
+        orange.set_low().expect("failed to set orange LED to high");
+        red.set_high().expect("failed to set red LED to high");
+        delay(3000);
+        red.set_low().expect("failed to set red LED to high");
+        blue.set_high().expect("failed to set blue LED to high");
+        delay(3000);
+    }
+}
